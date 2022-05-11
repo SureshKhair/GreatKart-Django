1d5d871c9907cd5229628d383ca47f4c58a4b3dc
sureshkhair <harichandrasuresh@gmail.com>
2022-05-11 14:30:57 +0530

    added cadr

--- carts/urls.py
+++ carts/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.cart,name='cart'),
     path('add_cart/<int:product_id>', views.add_cart,name='add_cart'),
+    path('remove_cart/<int:product_id>', views.remove_cart,name='remove_cart'),
+    path('remove_cartitem/<int:product_id>', views.remove_cartitem,name='remove_cartitem'),
     
 ]
